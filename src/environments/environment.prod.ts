a90874422ce7e4ce9a9ff2ee2c5b7d9f2abe0580
hexatester <revolusi147id@gmail.com>
2020-11-25 20:05:46 +0700

    initial environment setting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  loginUrl: "https://elearning.ut.ac.id/login/token.php",
+  wsUrl: "https://elearning.ut.ac.id/webservice/rest/server.php",
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  loginUrl: "https://school.moodledemo.net/login/token.php",
+  wsUrl: "https://school.moodledemo.net/webservice/rest/server.php",
 };
 
 /*
